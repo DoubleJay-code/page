596ea9458c3ce485547b59ed428f8536feec56a3
Artem <artemmakarov1355@gmail.com>
2022-11-11 14:59:07 +0600

    post done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Post } from './components/Post';
 
 const App: React.FC = () => {
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IPost } from '../types/interface';
 
 export const Post: React.FC = () => {
+  const [post5, setPost5] = useState<IPost>({});
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/5')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setPost5(json);
+      });
+  }, []);
   return (
     <div className="post">
       <h2 className="post__title">Post</h2>
@@ -9,9 +19,9 @@ export const Post: React.FC = () => {
           src="https://inventure.com.ua/img/thumb.990.660/upload/pic2022-1H/nft-ukraine-monkey.jpg"
           alt="Something wrong"
         />
-        <div>
-          <h3>title</h3>
-          <p>body</p>
+        <div className="post__block">
+          <h3>{post5.title}</h3>
+          <p>{`${post5.body}${post5.body}${post5.body}`}</p>
         </div>
       </div>
     </div>
